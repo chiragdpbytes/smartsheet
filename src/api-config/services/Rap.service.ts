c0cfd6698cb34c12ddeb60e9557293e7d53d39a5
Chirag Patel <chirag.patel@bytestechnolab.com>
2024-01-12 17:57:37 +0530

    env update

--- src/api-config/services/Rap.service.ts
+++ src/api-config/services/Rap.service.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 // const API_BASE_URL = 'https://dev-dmsapi-h4tux5ulqa-uc.a.run.app/api/v1';
-// const API_BASE_URL = 'https://api-smartsheet.btdemo.biz';
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://api-smartsheet.btdemo.biz';
 
 const apiService = axios.create({
   baseURL: API_BASE_URL,
